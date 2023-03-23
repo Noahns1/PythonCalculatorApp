98255fb54b4e3617779a659053ef9cef7b492d68
NoahSchlaupitzn <noahschlaupitz@gmail.com>
2023-03-22 21:47:30 -0400

    trying to add buttons

--- main.py
+++ main.py
@@ -1,39 +1,56 @@
 import tkinter as tk
 
-LARGE_FONT_STYLE = ("Arial", 40, "bold")
-SMALL_FONT_STYLE = ("Arial", 16)
+# constants
+LARGE_FONT = ("Comic Sans", 40, "bold")
+SMALL_FONT = ("Comic Sans", 16)
+WHITE = "#FFFFFF"
 LIGHT_GRAY = "#F5F5F5"
-LABEL_COLOR = "#25265E"
+LABEL_COLOR = "#19BDFF"
 
 class Calculator:
     def __init__(self):
+        # Create window
         self.window = tk.Tk()
         self.window.geometry("375x667")
         self.window.resizable(0, 0)
         self.window.title("Calculator")
 
+        # Create display
         self.total_expression = "0"
         self.current_expression = "0"
-        self.display_frame = self.create_display_frame()
+        self.display_frame = self.create_display()
+        self.total, self.expression = self.create_display_frame()
 
-        self.total_label, self.label = self.create_display_labels()
+        # Create buttons
+        self.digits = {
+            7: (1, 1), 8: (1, 2), 9: (1, 3),
+            4: (2, 1), 5: (2, 2), 6: (2, 3),
+            1: (3, 1), 2: (3, 2), 3: (3, 3),
+            0: (4, 2), '.': (4, 1)
+        }
+        self.create_digit_buttons()
         self.buttons_frame = self.create_buttons_frame()
 
-    def create_display_frame(self):
+    def create_display(self):
         frame = tk.Frame(self.window, height=221, bg=LIGHT_GRAY)
         frame.pack(expand=True, fill="both")
         return frame
 
-    def create_display_labels(self):
-        total_label = tk.Label(self.display_frame, text=self.total_expression, anchor=tk.E, bg=LIGHT_GRAY,
-                               fg=LABEL_COLOR, padx=24, font=SMALL_FONT_STYLE)
-        total_label.pack(expand=True, fill="both")
+    def create_display_frame(self):
+        total = tk.Label(self.display_frame, text=self.total_expression, anchor=tk.E, bg=LIGHT_GRAY,
+                               fg=LABEL_COLOR, padx=24, font=SMALL_FONT)
+        total.pack(expand=True, fill="both")
 
-        label = tk.Label(self.display_frame, text=self.current_expression, anchor=tk.E, bg=LIGHT_GRAY,
-                         fg=LABEL_COLOR, padx=24, font=LARGE_FONT_STYLE)
-        label.pack(expand=True, fill="both")
+        expression = tk.Label(self.display_frame, text=self.current_expression, anchor=tk.E, bg=LIGHT_GRAY,
+                         fg=LABEL_COLOR, padx=24, font=LARGE_FONT)
+        expression.pack(expand=True, fill="both")
 
-        return total_label, label
+        return total, expression
+
+    def create_digit_buttons(self):
+        for digit, grid_value in self.digits.items():
+            button = tk.Button(self.buttons_frame, text=str(digit), bg=WHITE, fg=LABEL_COLOR)
+            button.grid(row=grid_value[0], column=grid_value[1])
 
     def create_buttons_frame(self):
         frame = tk.Frame(self.window)
