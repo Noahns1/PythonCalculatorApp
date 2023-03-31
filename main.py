eba507c2f6af99c4f05ab07cffb14afa8d91e99a
NoahSchlaupitzn <noahschlaupitz@gmail.com>
2023-03-31 09:30:16 -0400

    fully functional

--- main.py
+++ main.py
@@ -1,64 +1,226 @@
+# By: Noah Schlaupitz
+# Calculator App
+import os
 import tkinter as tk
+from collections import namedtuple
+from PIL import Image
+import imagehash as ih
+import hashlib
+from os import listdir
 
-# constants
-LARGE_FONT = ("Comic Sans", 40, "bold")
-SMALL_FONT = ("Comic Sans", 16)
+# --- Constants ---
+# notes
+# add specific error funtionality
+# truncate expression
+
+
+# Fonts
+EXP_FONT = ("Comic Sans", 48, "bold")
+TTL_FONT = ("Comic Sans", 16)
+DIGITS_FONT = ("Comic Sans", 24, "bold")
+SYMBOLS_FONT = ("Comic Sans", 24)
+
+# Colors
+DARK_BLUE = '#282A36'
 WHITE = "#FFFFFF"
-LIGHT_GRAY = "#F5F5F5"
-LABEL_COLOR = "#19BDFF"
+LIGHT_BLUE = "#44475A"
+LIGHT_GRAY = "#A7A6BA"
+LABEL_COLOR = "#000000"
+BTN_COLOR = "#6272A4"
+
+# Numbers
+# Seven = hashlib.md5(Image.open('buttons/Seven.png'))
+# Seven.update()
+
+# note: crtl+/ will mass comment
+# One = ih.average_hash(Image.open('buttons/One.png'))
+# Two = ih.average_hash(Image.open('buttons/Two.png'))
+# Three = ih.average_hash(Image.open('buttons/Three.png'))
+# Four = ih.average_hash(Image.open('buttons/Four.png'))
+# Five = ih.average_hash(Image.open('buttons/Five.png'))
+# Six = ih.average_hash(Image.open('buttons/Six.png'))
+# Seven = ih.average_hash(Image.open('buttons/Seven.png'))
+# Eight = ih.average_hash(Image.open('buttons/Eight.png'))
+# Nine = ih.average_hash(Image.open('buttons/Nine.png'))
+# Zero = ih.average_hash(Image.open('buttons/Zero.png'))
 
 class Calculator:
     def __init__(self):
         # Create window
         self.window = tk.Tk()
-        self.window.geometry("375x667")
-        self.window.resizable(0, 0)
+        self.window.geometry("350x500")
+        # below disables window resize
+        # self.window.resizable(0, 0) # used to stop the window from being resized
         self.window.title("Calculator")
 
+        # --trying to adjust frame color--
+        # found out that this can't be changed and is only adjusted by
+        # window settings
+        # self.window.configure(bg="#000000")
+        # frame_color = tk.Frame(self.window, bg='#000000')
+        # frame_color.pack()
+
         # Create display
-        self.total_expression = "0"
-        self.current_expression = "0"
-        self.display_frame = self.create_display()
-        self.total, self.expression = self.create_display_frame()
+        self.total_expression = ""
+        self.current_expression = ""
+        self.display = self.display_frame()
+        self.total, self.expression = self.create_display()
 
         # Create buttons
+        # The error, I believe was because I was testing the picture with only one
+        # I believe I need to resize the pictures now for them to work
         self.digits = {
             7: (1, 1), 8: (1, 2), 9: (1, 3),
             4: (2, 1), 5: (2, 2), 6: (2, 3),
             1: (3, 1), 2: (3, 2), 3: (3, 3),
             0: (4, 2), '.': (4, 1)
         }
-        # this errored out because button_frame needed to be above
+        # This doesn't work, it only expects an iterable or a string
+        # self.Digits = namedtuple("Digits", self.digits.keys())(*self.digits.values())
+        # math symbols
+        self.operations = {"/": "\u00F7", "*": "\u00D7", "-": "-", "+": "+"}
+
+        # This errored out because button_frame needed to be above
         # self.create_buttons_frame
         # Note for error doc:
-        # if you assign an attribute to a method, if but be assigned above the method
-        self.buttons_frame = self.create_buttons_frame()
-        self.create_digit_buttons()
+        # Ff you assign an attribute to a method, if but be assigned above the method
+        self.buttons_frame = self.buttons_frame()
 
-    def create_display(self):
+        # Making the buttons fill into the screen
+        for b in range(1, 5):
+            self.buttons_frame.rowconfigure(b, weight=1)
+            self.buttons_frame.columnconfigure(b, weight=1)
+
+        # Buttons
+        self.digit_buttons()
+        self.operater_buttons()
+        self.special_buttons()
+
+        # button images ---unused ---
+        # self.pngList = {
+        #     1: "buttons/One.png", 2: "buttons/Two.png", 3: "buttons/Three.png",
+        #     4: "buttons/Four.png", 5: "buttons/Five.png", 6: "buttons/Six.png",
+        #     7: "buttons/Seven.png", 8: "buttons/Eight.png", 9: "buttons/Nine.png",
+        #     0: "buttons/Zero.png"
+        # }
+
+    # Buttons
+    def special_buttons(self):
+        self.clear_button()
+        self.equals_button()
+        self.sqrt_button()
+        self.square_button()
+
+    # Creating display for numbers and expression
+    def display_frame(self):
         frame = tk.Frame(self.window, height=221, bg=LIGHT_GRAY)
         frame.pack(expand=True, fill="both")
         return frame
 
-    def create_display_frame(self):
-        total = tk.Label(self.display_frame, text=self.total_expression, anchor=tk.E, bg=LIGHT_GRAY,
-                         fg=LABEL_COLOR, padx=24, font=SMALL_FONT)
-        total.pack(expand=True, fill="both")
+    #
+    def create_display(self):
+        ttl = tk.Label(self.display, text=self.total_expression, anchor=tk.E, bg=LIGHT_GRAY,
+                       fg=LABEL_COLOR, padx=24, font=TTL_FONT)
+        ttl.pack(expand=True, fill="both")
 
-        expression = tk.Label(self.display_frame, text=self.current_expression, anchor=tk.E, bg=LIGHT_GRAY,
-                              fg=LABEL_COLOR, padx=24, font=LARGE_FONT)
-        expression.pack(expand=True, fill="both")
-        return total, expression
+        exp = tk.Label(self.display, text=self.current_expression, anchor=tk.E, bg=LIGHT_GRAY,
+                       fg=LABEL_COLOR, padx=24, font=EXP_FONT)
+        exp.pack(expand=True, fill="both")
+        return ttl, exp
 
-    def create_digit_buttons(self):
+    def add_to_expression(self, value):
+        self.current_expression += str(value)
+        self.update_label()
+
+    def digit_buttons(self):
         for digit, grid_value in self.digits.items():
-            button = tk.Button(self.buttons_frame, text=str(digit), bg=WHITE, fg=LABEL_COLOR)
+            button = tk.Button(self.buttons_frame, text=digit, bg=WHITE, fg=BTN_COLOR,
+                               font=DIGITS_FONT, borderwidth=0,
+                               command=lambda dgt=digit: self.add_to_expression(dgt))
             button.grid(row=grid_value[0], column=grid_value[1], sticky=tk.NSEW)
 
-    def create_buttons_frame(self):
+    def append_operator(self, operator):
+        self.current_expression += operator
+        self.total_expression += self.current_expression
+        self.current_expression = ''
+        self.update_total_label()
+        self.update_label()
+
+    def operater_buttons(self):
+        x = 0
+        for operator, symbol in self.operations.items():
+            button = tk.Button(self.buttons_frame, text=symbol, bg=DARK_BLUE, fg=BTN_COLOR,
+                               font=SYMBOLS_FONT, borderwidth=0,
+                               command=lambda opt=operator: self.append_operator(opt))
+            button.grid(row=x, column=4, sticky=tk.NSEW)
+            x += 1
+
+    def clear(self):
+        self.current_expression = ''
+        self.total_expression = ''
+        self.update_label()
+        self.update_total_label()
+
+    def square(self):
+        self.current_expression = str(eval(f"{self.current_expression}**2"))
+        self.update_label()
+
+    def clear_button(self):
+        button = tk.Button(self.buttons_frame, text="AC", bg=DARK_BLUE, fg=BTN_COLOR,
+                           font=SYMBOLS_FONT, borderwidth=0,
+                           command=self.clear)
+        button.grid(row=0, column=1, sticky=tk.NSEW)
+
+    def square_button(self):
+        button = tk.Button(self.buttons_frame, text="x\u00b2", bg=DARK_BLUE, fg=BTN_COLOR,
+                           font=SYMBOLS_FONT, borderwidth=0,
+                           command=self.square)
+        button.grid(row=0, column=2, sticky=tk.NSEW)
+
+    def sqrt(self):
+        self.current_expression = str(eval(f"{self.current_expression}**.5"))
+        self.update_label()
+
+    def sqrt_button(self):
+        button = tk.Button(self.buttons_frame, text="\u221ax", bg=DARK_BLUE, fg=BTN_COLOR,
+                           font=SYMBOLS_FONT, borderwidth=0,
+                           command=self.sqrt)
+        button.grid(row=0, column=3, sticky=tk.NSEW)
+
+    def evaluate(self):
+        self.total_expression += self.current_expression
+        self.update_total_label()
+        # adding exception handling exceptions that won't compute
+        try:
+            self.current_expression = str(eval(self.total_expression))
+            self.total_expression = ''
+        except Exception as e:
+            self.current_expression = "ERROR"
+            # I used "ERROR" it resulted in "ERRORERROR", but with a blank string
+            # it passed the error message and turned the total expression blank
+            self.total_expression = ""
+        self.update_label()
+ 
+    def equals_button(self):
+        button = tk.Button(self.buttons_frame, text="=", bg=LIGHT_BLUE, fg=BTN_COLOR,
+                           font=SYMBOLS_FONT, borderwidth=0,
+                           command=self.evaluate)
+        button.grid(row=4, column=3, columnspan=2, sticky=tk.NSEW)
+
+    def buttons_frame(self):
         frame = tk.Frame(self.window)
         frame.pack(expand=True, fill="both")
         return frame
+
+    def update_total_label(self):
+        expression = self.total_expression
+        for operator, symbol in self.operations.items():
+            expression = expression.replace(operator, f'{symbol}')
+        self.total.config(text=expression)
+
+    def update_label(self):
+        # updating the label and truncating it to 10
+        self.expression.config(text=self.current_expression[:10])
 
     def run(self):
         self.window.mainloop()
@@ -67,9 +229,3 @@ class Calculator:
 if __name__ == "__main__":
     calc = Calculator()
     calc.run()
-
-
-
-
-
-
